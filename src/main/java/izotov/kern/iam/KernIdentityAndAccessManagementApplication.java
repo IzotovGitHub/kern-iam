dbafaae31008477b9d931be31214603e29ffea1f
izotov_a <aleksandr.izotov2020@gmail.com>
2026-01-02 15:50:03 +0300

    init

---
+++ src/main/java/izotov/kern/iam/KernIdentityAndAccessManagementApplication.java
@@ -0,0 +1,12 @@
+package izotov.kern.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KernIdentityAndAccessManagementApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(KernIdentityAndAccessManagementApplication.class, args);
+    }
+}
