4596d2f2562be0d9a628e0d98bbe734ab3fe7301
izotov_a <aleksandr.izotov2020@gmail.com>
2026-01-02 19:55:42 +0300

    add liquibase configuration

---
+++ src/main/java/izotov/kern/iam/config/LiquibaseConfig.java
@@ -0,0 +1,30 @@
+package izotov.kern.iam.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig {
+    
+    @Value("${spring.liquibase.enabled:false}")
+    private boolean enabled;
+    @Value("${spring.liquibase.change-log}")
+    private String changeLog;
+    @Value("${spring.liquibase.drop-first:false}")
+    private boolean dropFirst;
+    
+    
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setShouldRun(enabled);
+        liquibase.setChangeLog(changeLog);
+        liquibase.setDropFirst(dropFirst);
+        return liquibase;
+    }
+}
